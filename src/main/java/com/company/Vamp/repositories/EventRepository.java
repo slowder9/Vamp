32a642744a94624b91753423667fc12bf9f9d98f
Adam Locklear <adamlocklear@gmail.com>
2017-10-13 13:02:30 -0400

    tesing repo push

---
+++ src/main/java/com/company/Vamp/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.company.Vamp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.awt.*;
+
+public interface EventRepository {
+extends CrudRepository<Event, Integer>
+}
