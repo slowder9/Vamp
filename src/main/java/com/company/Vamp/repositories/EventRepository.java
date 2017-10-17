47b6d5f170beca18eb61b04ebfade8a87260bd4e
Savannah Lowder <savannahlowder@Savannahs-MacBook-Air.local>
2017-10-17 16:40:08 -0400

    merge conflict

--- src/main/java/com/company/Vamp/repositories/EventRepository.java
+++ src/main/java/com/company/Vamp/repositories/EventRepository.java
@@ -1,10 +1,10 @@
 package com.company.Vamp.repositories;
 
-import com.company.Vamp.models.Vamp;
+import com.company.Vamp.models.Event;
 import org.springframework.data.repository.CrudRepository;
 
 import java.awt.*;
 
 public interface EventRepository
-        extends CrudRepository<Vamp, Integer> {
+        extends CrudRepository<Event, Integer> {
 }
