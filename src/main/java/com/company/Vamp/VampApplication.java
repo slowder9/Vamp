0827d0c7d08ce2cc0879b36811bea24bfbddb178
Adam Locklear <adamlocklear@gmail.com>
2017-10-17 14:55:55 -0400

    Deleting Events

--- src/main/java/com/company/Vamp/VampApplication.java
+++ src/main/java/com/company/Vamp/VampApplication.java
@@ -2,8 +2,10 @@ package com.company.Vamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class VampApplication {
 
 	public static void main(String[] args) {
